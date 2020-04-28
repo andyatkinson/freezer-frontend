88b65831512bb735c421ead2626b9074f1b71304
Andy Atkinson <andyatkinson@gmail.com>
2020-04-28 13:24:39 -0500

    Handle empty input with alert

--- react-ui/src/containers/AddItem.js
+++ react-ui/src/containers/AddItem.js
@@ -18,11 +18,18 @@ const AddItem = ({ dispatch }) => {
     btn.setAttribute('value', 'Saving...')
   }
 
+  const handleClose = function() {
+    document.getElementById('empty').hide();
+  }
+
   const saveItem = function (event) {
     // synthetic event
     if (!input.value.trim()) {
+      document.getElementById('empty').show();
       return
     }
+
+    setSubmitState()
 
     const newItem = {name: input.value, addedOn: today}
     const hostname = window.location.hostname
@@ -58,11 +65,17 @@ const AddItem = ({ dispatch }) => {
   return (
     <div>
       <ons-page>
+        <ons-alert-dialog id="empty">
+          <div class="alert-dialog-content">
+            Please enter item content
+          </div>
+          <ons-alert-dialog-button onClick={handleClose.bind(this)}>OK</ons-alert-dialog-button>
+        </ons-alert-dialog>
         <ons-toolbar>
           <div className="center">Add Item</div>
         </ons-toolbar>
         <div style={containerStyle}>
-          <form onSubmit={(e) => {saveItem(); e.preventDefault(); setSubmitState()}}>
+          <form onSubmit={(e) => {saveItem(); e.preventDefault();}}>
               <ons-input modifier="underbar"
                 placeholder="Lasagna w/ Meat (Servings: 4)..."
                 float
